b30c0efa9e4db5c8ab5eba44b7b8b1ee71d2abbf
Laura Schulz <lrs8810@gmail.com>
2020-01-23 14:34:12 -0700

    Add Direwolf and Stark classes and test

---
+++ exercises/direwolf.js
@@ -0,0 +1,26 @@
+class Direwolf {
+  constructor(name, home, size) {
+    this.name = name;
+    this.home = home || 'Beyond the Wall';
+    this.size = size || 'Massive';
+    this.starksToProtect = [];
+    this.huntsWhiteWalkers = true;
+  }
+
+  protect(stark) {
+    if (this.home === stark.location) {
+      if (this.starksToProtect.length < 2){
+        this.starksToProtect.push(stark);
+        stark.safe = true;
+        this.huntsWhiteWalkers = false;
+      }
+    }
+  }
+
+  leave(stark) {
+    this.starksToProtect.shift(stark);
+    stark.safe = false;
+  }
+}
+
+module.exports = Direwolf;
