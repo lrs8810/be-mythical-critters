b4b9d75720bb35b0add0a889d49e8bc3de044c14
Laura Schulz <lrs8810@gmail.com>
2020-01-25 16:40:39 -0700

    Add completed sphinx

---
+++ exercises/sphinx.js
@@ -0,0 +1,30 @@
+var _ = require('lodash');
+
+class Sphinx {
+  constructor() {
+    this.riddles = [];
+    this.heroesEaten = 0;
+  }
+
+  collectRiddle(riddle) {
+    this.riddles.push(riddle)
+    if (this.riddles.length > 3){
+      this.riddles.shift()
+    }
+  }
+
+  attemptAnswer(answer) {
+    var correct_answers = _.remove(this.riddles, function(riddle) {
+      return riddle.answer === answer;
+    });
+    if (this.riddles.length === 0) {
+      return `PSSSSSSS THIS HAS NEVER HAPPENED, HOW DID YOU KNOW THE ANSWER WAS "${answer}"???`
+    } else if (correct_answers.length > 0) {
+      return 'That wasn\'t that hard, I bet you don\'t get the next one'
+    } else {
+      this.heroesEaten++;
+    }
+  }
+}
+
+module.exports = Sphinx;
